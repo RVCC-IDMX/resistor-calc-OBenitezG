13963afb11fe3b6c3f545c6cd41b00f93f944d1b
Obdulio Benitez Garcia <112982014+OBenitezG@users.noreply.github.com>
2023-11-12 16:39:27 -0500

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 import { getResistorOhms } from './resistor.js';
+
+const selection= document.querySelector('.selection');
+const bands = { color1: 'red', color2: 'green', multiplier: 'violet', tolerance: 'gold' };
+
+selection.addEventListener('click', (event) => {
+    const slot = event.target.parentNode.id;
+    if (['color0', 'color1', 'color2', 'color3'].includes(slot)) {
+        const color = event.target.classList[1];
+        bands[slot] = color;
+        resistorUpdate(`b${slot.replace(/\D/g, '')}`, color);
+    }
+});
+
+function resistorUpdate(id, color) {
+    document.getElementById(id).style.backgroundColor = `var(--${color})`;
+    document.querySelector('.answer').textContent = getResistorOhms(bands);
+}
